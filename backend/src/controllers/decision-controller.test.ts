615869aebbfcfc262b2aadf39eba0a03c20782ee
Idan Izicovich <idanizi@gmail.com>
2023-05-07 03:18:49 +0300

    tests

--- backend/src/controllers/decision-controller.test.ts
+++ backend/src/controllers/decision-controller.test.ts
@@ -1,13 +1,40 @@
 import {applyRulesToSummariseLoanRequest} from "@src/controllers/decision-controller";
 import {balances} from "@src/services/accounting-software.mock";
-import {LoanRequest} from "@src/types";
+import {BalanceSheet, LoanRequest} from "@src/types";
+import dayjs from "dayjs";
 
 describe('decision controller', function () {
-    it('should apply rules to requested loan', function () {
-        const act = applyRulesToSummariseLoanRequest(balances, 1200)
-        expect(act).toBe<LoanRequest>({
+    it('should apply rules to requested loan when loan is bigger then average asserts value', () => {
+        const act = applyRulesToSummariseLoanRequest(balances, 1200000)
+        expect(act).toEqual<LoanRequest>({
             balanceSheet: balances,
             preAssessment: 60,
         })
     });
+
+    it('should apply rules to request loan when loan is smaller then average assets value', () => {
+        const act = applyRulesToSummariseLoanRequest(balances, 120)
+        expect(act).toEqual<LoanRequest>({
+            balanceSheet: balances,
+            preAssessment: 100,
+        })
+    })
+
+    it('should return 20 when no profit and average assets value is smaller than request value', () => {
+        const now = dayjs()
+        let balances: BalanceSheet = [1, 2, 3, 4, 5, 6].map(x => {
+            const date = now.subtract(x, 'month')
+            return {
+                profitOrLoss: -100,
+                assetsValue: -1,
+                month: date.month(),
+                year: date.year(),
+            }
+        })
+        const act = applyRulesToSummariseLoanRequest(balances, 1200000)
+        expect(act).toEqual<LoanRequest>({
+            balanceSheet: balances,
+            preAssessment: 20,
+        })
+    })
 });
