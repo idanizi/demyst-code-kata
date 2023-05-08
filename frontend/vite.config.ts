caf80a48991a8b7dda653147514aa99f90af73b4
Idan Izicovich <idanizi@gmail.com>
2023-05-08 20:56:14 +0300

    code cleanup

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import {} from 'vitest'
-import react from '@vitejs/plugin-react'
+import {defineConfig} from 'vite';
+import 'vitest';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
