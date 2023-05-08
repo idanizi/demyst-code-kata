6a5130b482a6d8389da52a8d9119307acc4b2f31
Idan Izicovich <idanizi@gmail.com>
2023-05-08 23:17:25 +0300

    footer

--- frontend/src/components/Page.tsx
+++ frontend/src/components/Page.tsx
@@ -1,9 +1,17 @@
 import {FC, PropsWithChildren} from "react";
 
+const email = 'idanizi@gmail.com';
+
 export const Page: FC<PropsWithChildren> = ({children}) => {
+
     return (
         <main>
-            {children}
+            <article>
+                {children}
+            </article>
+            <footer>
+                Made with ❤️ by Idan Izicovich <a href={'mailto:' + email}>{email}</a>
+            </footer>
         </main>
     )
 }
